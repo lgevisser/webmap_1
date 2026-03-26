4837ac625785ecb1cca256c2d8044f2fae658be2
Leo Gevisser <leo@gevisser.com>
2026-03-26 13:47:30 +0200

    add Copenhagen noise and bike data

--- map.js
+++
@@ -1,7 +0,0 @@
-'use strict'        // let the browser know we're serious
-
-// debug statement letting us know the file is loaded
-console.log('Loaded map.js')
-
-// your mapbox token
-mapboxgl.accessToken = 'YOUR TOKEN HERE BETWEEN THE QUOTES'
